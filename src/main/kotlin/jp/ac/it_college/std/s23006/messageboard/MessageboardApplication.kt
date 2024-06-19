32621e49840cfafa894f9e6a5378f997dc47af79
itc-s23006 <s23006@std.it-college.ac.jp>
2024-06-19 10:06:53 +0900

    データベースの準備できた。

---
+++ src/main/kotlin/jp/ac/it_college/std/s23006/messageboard/MessageboardApplication.kt
@@ -0,0 +1,11 @@
+package jp.ac.it_college.std.s23006.messageboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MessageboardApplication
+
+fun main(args: Array<String>) {
+	runApplication<MessageboardApplication>(*args)
+}
---
+++ src/test/kotlin/jp/ac/it_college/std/s23006/messageboard/MessageboardApplicationTests.kt
@@ -0,0 +1,13 @@
+package jp.ac.it_college.std.s23006.messageboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MessageboardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
