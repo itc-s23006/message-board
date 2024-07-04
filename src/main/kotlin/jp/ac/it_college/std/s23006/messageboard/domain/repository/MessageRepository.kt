66c4718930ced8d4e826bd64a864083d8991972f
itc-s23006 <s23006@std.it-college.ac.jp>
2024-07-04 14:11:58 +0900

    ServerSide Kotlin(課題作成)
    ServerSide Kotlin 課題(修正)
    
    Repositoryを修正。
    
    7/4

--- src/main/kotlin/jp/ac/it_college/std/s23006/messageboard/domain/repository/MessageRepository.kt
+++ src/main/kotlin/jp/ac/it_college/std/s23006/messageboard/domain/repository/MessageRepository.kt
@@ -4,8 +4,7 @@ import jp.ac.it_college.std.s23006.messageboard.domain.model.Message
 
 interface MessageRepository {
     fun findByThreadId(threadId: Long): List<Message>
-    fun findById(id: Long): Message?
-    fun create(message: Message): Message
-    fun update(message: Message): Message
-    fun deleteById(id: Long)
+    fun save(threadId: Long, message: String, userId: Long): Message
+    fun update(id: Long, message: String, userId: Long): Message
+    fun delete(id: Long, userId: Long): Message
 }
--- src/main/kotlin/jp/ac/it_college/std/s23006/messageboard/domain/repository/ThreadRepository.kt
+++ src/main/kotlin/jp/ac/it_college/std/s23006/messageboard/domain/repository/ThreadRepository.kt
@@ -1,9 +1,9 @@
 package jp.ac.it_college.std.s23006.messageboard.domain.repository
 
-import org.springframework.stereotype.Repository
-
-@Repository
 interface ThreadRepository {
     fun findAll(): List<Thread>
-    fun findById(id: Long): Thread?
+    fun findById(id: Long): Thread
+    fun save(title: String, message: String, userId: Long): Long
+    fun updateTitle(id: Long, title: String, userId: Long): Thread
+    fun delete(id: Long, userId: Long): Thread
 }
