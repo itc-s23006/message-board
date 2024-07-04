be9b4bc42da350a0e443b68b4f2155fe07cad859
itc-s23006 <s23006@std.it-college.ac.jp>
2024-07-04 14:17:55 +0900

    ServerSide Kotlin(課題作成)
    ServerSide Kotlin 課題(修正)
    
    daoを修正。
    
    7/4

--- src/main/kotlin/jp/ac/it_college/std/s23006/messageboard/infrastructure/database/dao/MessagesTable.kt
+++ src/main/kotlin/jp/ac/it_college/std/s23006/messageboard/infrastructure/database/dao/MessagesTable.kt
@@ -3,11 +3,11 @@ package jp.ac.it_college.std.s23006.messageboard.infrastructure.database.dao
 import org.jetbrains.exposed.dao.id.LongIdTable
 import org.jetbrains.exposed.sql.kotlin.datetime.datetime
 
-object MessagesTable : LongIdTable() {
-    val threadId = long("thread_id")
-    val userId = long("user_id")
+object MessagesTable : LongIdTable("messages") {
+    val threadId = long("thread_id").index()
+    val userId = long("user_id").index()
     val message = text("message")
     val postedAt = datetime("posted_at")
     val updatedAt = datetime("updated_at")
-    val deleted = bool("deleted")
+    val deleted = bool("deleted").default(false)
 }
--- src/main/kotlin/jp/ac/it_college/std/s23006/messageboard/infrastructure/database/dao/ThreadsTable.kt
+++ src/main/kotlin/jp/ac/it_college/std/s23006/messageboard/infrastructure/database/dao/ThreadsTable.kt
@@ -1,26 +1,11 @@
 package jp.ac.it_college.std.s23006.messageboard.infrastructure.database.dao
 
-import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.dao.id.LongIdTable
 import org.jetbrains.exposed.sql.kotlin.datetime.datetime
-import jp.ac.it_college.std.s23006.messageboard.domain.model.Thread
 
-object ThreadsTable : Table("threads") {
-    val id = long("id").autoIncrement()
+object ThreadsTable : LongIdTable("threads") {
     val title = varchar("title", 255)
     val createdAt = datetime("created_at")
-
-    override val primaryKey = PrimaryKey(id)
-
-    fun selectAll(): List<ResultRow> {
-        return ThreadsTable.selectAll().toList()
-    }
-
-    fun ResultRow.toModel(): Thread {
-        return Thread(
-            id = this[ThreadsTable.id],
-            title = this[ThreadsTable.title],
-            createdAt = this[ThreadsTable.createdAt]
-        )
-    }
+    val updatedAt = datetime("updated_at")
+    val deleted = bool("deleted").default(false)
 }
